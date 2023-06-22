f6c1bf447c6fbe60b8456891664b3ecef895d113
EswarSk <skeswar2@gmail.com>
2023-06-22 19:59:55 -0400

    implement needs_service function

--- engine/battery.py
+++ engine/battery.py
@@ -7,19 +7,23 @@ class Battery(Serviceable, ABC):
         pass
 
 class SpindlerBattery(Battery):
+    SERVICE_INTERVAL_YEARS = 2
+
     def __init__(self, last_service_date, current_date):
         self.last_service_date = last_service_date
         self.current_date = current_date
 
     def needs_service(self) -> bool:
-        # Add logic
-        pass
+        years_since_last_service = self.current_date.year - self.last_service_date.year
+        return years_since_last_service >= self.SERVICE_INTERVAL_YEARS
 
 class NubbinBattery(Battery):
+    SERVICE_INTERVAL_YEARS = 4
+
     def __init__(self, last_service_date, current_date):
         self.last_service_date = last_service_date
         self.current_date = current_date
 
     def needs_service(self) -> bool:
-        # Add logic
-        pass
+        years_since_last_service = self.current_date.year - self.last_service_date.year
+        return years_since_last_service >= self.SERVICE_INTERVAL_YEARS
--- engine/engine.py
+++ engine/engine.py
@@ -7,27 +7,28 @@ class Engine(Serviceable, ABC):
         pass
 
 class CapuletEngine(Engine):
+    SERVICE_INTERVAL = 30000
+
     def __init__(self, last_service_mileage, current_mileage):
         self.last_service_mileage = last_service_mileage
         self.current_mileage = current_mileage
 
     def needs_service(self) -> bool:
-        # Add logic
-        pass
+        return self.current_mileage - self.last_service_mileage >= self.SERVICE_INTERVAL
 
 class SternmanEngine(Engine):
     def __init__(self, warning_light_on):
         self.warning_light_on = warning_light_on
 
     def needs_service(self) -> bool:
-        # Add logic 
-        pass
+        return self.warning_light_on
 
 class WilloughbyEngine(Engine):
+    SERVICE_INTERVAL = 60000
+
     def __init__(self, last_service_mileage, current_mileage):
         self.last_service_mileage = last_service_mileage
         self.current_mileage = current_mileage
 
     def needs_service(self) -> bool:
-        # Add logic
-        pass
+        return self.current_mileage - self.last_service_mileage >= self.SERVICE_INTERVAL
